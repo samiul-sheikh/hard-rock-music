903247cda91570a26241c5d8af745664e11ded4b
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-02-11 21:41:12 +0600

    show audio preview and call lyric function

--- app.js
+++ app.js
@@ -20,11 +20,18 @@ const displaySongs = songs => {
             <div class="col-md-9">
                 <h3 class="lyrics-name">${song.title}</h3>
                 <p class="author lead">Album by <span>${song.artist.name}</span></p>
+                <audio controls>
+                    <source src="${song.preview}" type="audio/mpeg">
+                </audio>
             </div>
             <div class="col-md-3 text-md-right text-center">
-                <button class="btn btn-success">Get Lyrics</button>
+                <button onclick="getLyric('${song.artist.name}','${song.title}')" class="btn btn-success">Get Lyrics</button>
             </div>
         `;
         songContainer.appendChild(songDiv);
-    });
+    })
+}
+
+const getLyric = (artist, title) => {
+    console.log(artist, title);
 }
